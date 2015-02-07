7e3bf52845b1aafcea87f71532ae4005c40e5fe8
john ko <johnsko89@gmail.com>
2015-02-06 21:03:02 -0500

    dont understand pythons flow control

---
+++ guessthenumber.py
@@ -0,0 +1,22 @@
+# guessing the number
+
+print "Guess the number from 1-100"
+
+guess = raw_input()
+
+numbers = range(1,101)
+import random
+the_number = (random.choice(numbers))
+
+counter = 0
+
+while True:
+    if guess > the_number:
+        counter += 1
+        print "Too high! Try Again."
+    if guess < the_number:
+        counter += 1
+        print "Too low! Try Again."
+    if guess == the_number:
+        print "Congratulations! You got it in {} guesses.".format(counter)
+        break
