922a6fb44e382aa05b3205a33045c8ea2e722d7a
john ko <johnsko89@gmail.com>
2015-02-11 01:51:46 -0500

    fixed guess the number

--- guessthenumber.py
+++ guessthenumber.py
@@ -2,25 +2,28 @@
 
 print "Guess the number from 1-100"
 
-guess = raw_input()
 
 numbers = range(1,101)
 import random
-the_number = (random.choice(numbers))
+the_number = int((random.choice(numbers)))
 
 counter = 0
 
 while True:
+    guess = int(raw_input())
     if guess > the_number:
         counter += 1
         print "Too high! Try Again."
-    if guess < the_number:
+    elif guess < the_number:
         counter += 1
         print "Too low! Try Again."
-    if guess == the_number:
+    elif guess == the_number:
         print "Congratulations! You got it in {} guesses.".format(counter)
         break
 
 # output infinitely prints out either too high, or too low
 # i don't get this flow control am i supposed to break the loop
 # will read more
+
+#fixed it because i was not getting an integer so it wasn't running through
+#the loops correctly!
